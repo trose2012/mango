3f1b42c926320ec0ce09ddd2dfb9fab383bf1c5d
Echo <github@3kh0.net>
2025-03-26 22:20:48 -0400

    cf updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      "onion-location":
+        "http://aqjeignx5ussinaa64hq3s2phleke6jmworiqe2tmlh4kctklyq6pjqd.onion",
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
   compatibilityDate: "2024-11-01",
   css: ["~/assets/main.css"],
   devtools: { enabled: true },
